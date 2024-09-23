561c0acd8e5e4a6125ae30f10c93d1fda806fc08
QuinnMurphy08 <qmurphy27@students.stab.org>
2024-09-23 14:58:44 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,95 @@
+
+function getShoe(){
+
+    //first, get the user inputs from the text boxes and save them to variables. 
+    var colorInput = document.getElementById("color").value;
+    var ageInput= document.getElementById("age").value;
+  var finalShoe;
+    //then, use those variables and some contitionals to determine what the price of the ticket should be
+  
+  if (=="" && Input==""){
+    console.log();
+  }
+  else if (=="" && Input==""){
+    console.log();
+  }
+  else if (=="" && Input==""){
+    console.log();
+  }
+  else if (=="" && Input==""){
+    console.log();
+  }
+  else if (=="" && Input==""){
+    console.log();
+  }
+  else if (=="" && Input==""){
+    console.log();
+  }
+  else if (=="" && Input==""){
+    console.log();
+  }
+  else if (=="" && Input==""){
+    console.log();
+  }
+  else if (=="" && Input==""){
+    console.log();
+  }
+  else if (=="" && Input==""){
+    console.log();
+  }
+  else if (=="" && Input==""){
+    console.log();
+  }
+  else if (=="" && Input==""){
+    console.log();
+  }
+  else if (=="" && Input==""){
+    console.log();
+  }
+  else if (=="" && Input==""){
+    console.log();
+  }
+  else if (=="" && Input==""){
+    console.log();
+  }
+  else if (=="" && Input==""){
+    console.log();
+  }
+  else if (=="" && Input==""){
+    console.log();
+  }
+  else if (=="" && Input==""){
+    console.log();
+  }
+  else if (=="" && Input==""){
+    console.log();
+  }
+  else if (=="" && Input==""){
+    console.log();
+  }
+  else if (=="" && Input==""){
+    console.log();
+  }
+  else if (=="" && Input==""){
+    console.log();
+  }
+  else if (=="" && Input==""){
+    console.log();
+  }
+  else if (=="" && Input==""){
+    console.log();
+  }
+  else (=="" && Input==""){
+    console.log();
+  }
+  
+  finalShoe= "<b>Color:</b>" + colorInput + "<br><b>Age:</b>" + ageInput;
+  
+  document.getElementById("output").innerHTML = ticketText;
+  document.getElementById("age").innerHTML = "";
+  document.getElementById("color").innerHTML = "";
+  
+    //Lastly, set the results to display on the screen. Here is an example, although this will just set the results to be an empty string (you'll need to edit what it equals).
+    
+    
+  }
